6ccdf8bd48502a3b343edf65079e16e65ce7fa4f
s6nch0s <sashka.vipka@gmail.com>
2021-12-28 23:49:03 +0200

    added some PEP

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ from keras import layers
 from keras.models import Sequential, load_model
 from keras.preprocessing.image import load_img, img_to_array
 import numpy as np
+
+
 batch_size = 32
 img_height = 180
 img_width = 180
+
 train_dg = keras.preprocessing.image.ImageDataGenerator(rescale=1./255, validation_split=0.2)
 train_dg = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
 train_generator = train_dg.flow_from_directory(
     'Large',
     class_mode='binary', seed=123)
+
 validation_generator = train_dg.flow_from_directory(
     'Large',
     class_mode='binary', seed=123)
+
 class_names = train_dg.class_names
 normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
 num_classes = len(class_names)
@@ -30,15 +36,20 @@ model = Sequential([
     layers.Dense(128, activation='relu'),
     layers.Dense(num_classes)
 ])
+
 model.compile(optimizer='adam',
               loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-epochs=10
+
+epochs = 10
+
 history = model.fit(
     train_ds,
     validation_data=val_ds,
     epochs=epochs
 )
+
 with open('model.h5', 'w') as f:
     f.close()
+
 model.save('model.h5')
